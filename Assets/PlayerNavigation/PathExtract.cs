9a6358a619070585fcd61a8f6c93ff84d508f575
risulkarimx1 <risulkarimx1@gmail.com>
2019-03-09 17:21:51 +0100

    [Navigation] Scene added with Player

---
+++ Assets/PlayerNavigation/PathExtract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathExtract : MonoBehaviour
+{
+    Mesh mesh;
+    public Vector3[] vertices;
+
+    public Vector3[] WorldPosition;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ExtractPath();
+    }
+    public void ExtractPath()
+    {
+        WorldPosition = new Vector3[vertices.Length];
+        mesh = GetComponent<MeshFilter>().sharedMesh;
+        vertices = mesh.vertices;
+        int index = 0;
+        foreach (var vertPosition in vertices)
+        {
+            WorldPosition[index++] = transform.TransformPoint(vertPosition);
+        }
+    }
+
+
+}
---
+++ Assets/PlayerNavigation/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 100;
+    [SerializeField] private Transform RaycastOrigin;
+    // Start is called before the first frame update
+
+    public PathExtract pathExtract;
+
+    void Start()
+    {
+        transform.position = pathExtract.WorldPosition[0];
+        InvokeRepeating("ChangePositionInvokeR",0,.1f);
+    }
+
+    private int currentVertexIndex = 0;
+    private Vector3 currentPosition;
+   
+    void ChangePositionInvokeR()
+    {
+        currentPosition = pathExtract.WorldPosition[currentVertexIndex];
+        currentVertexIndex = (currentVertexIndex+1) % pathExtract.vertices.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, currentPosition, Time.deltaTime * 10);
+
+        Ray ray = new Ray(RaycastOrigin.position, -transform.up);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+
+        {
+
+            var rot = Quaternion.FromToRotation(transform.up, hit.normal) *
+                   transform.rotation;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot,
+                Time.deltaTime*10);
+
+            var currPosition = transform.position;
+            currPosition.y = Mathf.Lerp(currPosition.y, hit.point.y,Time.deltaTime);
+            transform.position = currPosition;
+        }
+    }
+}
