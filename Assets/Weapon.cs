8db428e370cce71d89199457ba95b33a68c718d5
lara <larafercue@gmail.com>
2019-03-09 15:09:32 +0100

    Creating the Weapon prefab with behavior

---
+++ Assets/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject proyectile;
+    public Transform firingStartPoint;
+    public float cooldown = 0.5f;
+    private float _timer;
+
+    public void Attack()
+    {
+        if (_timer <= 0.0f)
+        {
+            Instantiate(proyectile, transform.position, transform.rotation);
+            _timer = cooldown;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer < 0.0f)
+            _timer = 0.0f;
+    }
+}
