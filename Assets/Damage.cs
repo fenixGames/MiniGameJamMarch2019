74e91d31cf8a6c48507116ba55929497564d8da9
lara <larafercue@gmail.com>
2019-03-09 16:02:24 +0100

    Creating the Damage system for a target on the scene

---
+++ Assets/Damage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public float damage = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health othersHealth = other.GetComponent<Health>();
+
+        if (othersHealth != null)
+        {
+            othersHealth.ApplyDamage(damage);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float hitPoints;
+
+    public void ApplyDamage(float damage)
+    {
+        hitPoints -= damage;
+
+        DeathEffect deathEffect = GetComponent<DeathEffect>();
+
+        if (deathEffect != null && hitPoints <= 0.0f)
+            deathEffect.Die();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
