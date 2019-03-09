4977921e6f47f2ad485a9e07741ca34c09de7a52
Simon <fate1990@googlemail.com>
2019-03-09 16:15:49 +0100

    imporeded vr stuff

---
+++ Assets/VRAttacking.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class VRAttacking : MonoBehaviour
+{
+    public SteamVR_Input_Sources input_Source;
+    public SteamVR_Action_Boolean steam_action1;
+    public SteamVR_Action_Boolean steam_action2;
+
+    public Weapon weapon1;
+    public Weapon weapon2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (steam_action1.GetState(input_Source))
+        {
+            Debug.Log("111111");
+           // weapon1.Attack();
+        }
+
+        if (steam_action2.GetState(input_Source))
+        {
+            Debug.Log("2222");
+
+            //  weapon2.Attack();
+        }
+
+
+
+    }
+}
